b34398b8e65ea98389d72af559949a0e327e159f
SidAgrawal-1212 <57342124+SidAgrawal-1212@users.noreply.github.com>
2020-02-06 13:02:25 +0530

    Update test.tf

--- test.tf
+++ test.tf
@@ -1,16 +1,17 @@
 # Terraform HCL
 
-provider "aws" {
-  region = "ap-south-1"
-  access_key = "AKIA2EQGHQQCG44OGYHC"
-  secret_key = "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"
+provider "github" {
+  token        = "86b2ec558029e5c1c696b161e84c236d97159759"
 }
 
-resource "aws_instance" "sidtest_ec2" {
-  ami           = "ami-07d0cf3af28718ef8"
-  instance_type = "t2.micro"
+resource "github_repository" "test_terraform" {
+  name        = "test_terraform"
+  description = "My awesome codebase"
 
-  tags = {
-    Name = "HelloTerraform"
+  private = true
+
+  template {
+    owner = "SidAgrawal-1212"
+    repo = "ansible_advance_homework"
   }
 }
