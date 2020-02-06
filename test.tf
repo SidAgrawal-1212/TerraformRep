c0cc04eeed65ed90eff648d0ec7fa29d5bc9f693
SidAgrawal-1212 <57342124+SidAgrawal-1212@users.noreply.github.com>
2020-02-06 14:14:59 +0530

    Update test.tf

--- test.tf
+++ test.tf
@@ -1,18 +1,8 @@
-# Terraform HCL
+data "http" "example" {
+  url = "https://checkpoint-api.hashicorp.com/v1/check/terraform"
 
-provider "github" {
-  token        = "86b2ec558029e5c1c696b161e84c236d97159759"
-  organization = "SdxWipro"
-}
-
-resource "github_repository" "test_terraform" {
-  name        = "test_terraform"
-  description = "My awesome codebase"
-
-  private = true
-
-  template {
-    owner = "SidAgrawal-1212"
-    repository = "ansible_advance_homework"
+  # Optional request headers
+  request_headers {
+    "Accept" = "application/json"
   }
 }
