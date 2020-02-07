3908298f89a6ae1fb478b46ba4fb5dbca511af8a
SidAgrawal-1212 <57342124+SidAgrawal-1212@users.noreply.github.com>
2020-02-07 12:03:12 +0530

    Update test.tf

--- test.tf
+++ test.tf
@@ -1,3 +1,10 @@
-data "http" "example" {
-  url = "https://checkpoint-api.hashicorp.com/v1/check/terraform"
+# Configure the GitHub Provider
+provider "github" {
+  token        = "86b2ec558029e5c1c696b161e84c236d97159759"
+  organization = "SdxWipro"
+}
+
+# Add a user to the organization
+resource "github_membership" "sid_user" {
+  # ...
 }
