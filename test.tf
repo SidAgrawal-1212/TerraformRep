e80cf0b1a5064668b61693fb5b6e51166f8493fe
SidAgrawal-1212 <57342124+SidAgrawal-1212@users.noreply.github.com>
2020-02-07 12:25:55 +0530

    Update test.tf

--- test.tf
+++ test.tf
@@ -1,12 +1,10 @@
 # Configure the GitHub Provider
 provider "github" {
   token        = "86b2ec558029e5c1c696b161e84c236d97159759"
-  organization = "SdxWipro"
+  individual = "true"
 
 }
 
-# Add a user to the organization
-resource "github_membership" "SdxWipro" {
-  username = "SomeUser"
-  # ...
+data "github_user" "SidAgrawal-1212" {
+  username = "SidAgrawal-1212"
 }
